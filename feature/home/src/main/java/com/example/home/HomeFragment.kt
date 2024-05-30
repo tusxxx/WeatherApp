a8bcc3dfff0d94edfd8fb7304232043682981941
Игнат Мустафаев <tusxdie@gmail.com>
2024-05-31 00:15:47 +0700

    Done

--- feature/home/src/main/java/com/example/home/HomeFragment.kt
+++ feature/home/src/main/java/com/example/home/HomeFragment.kt
@@ -16,6 +16,8 @@ import com.example.home.adapters.FoundCitiesAdapter
 import com.example.home.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.text.DateFormat
+import java.util.Calendar
 
 
 @AndroidEntryPoint
@@ -61,6 +63,9 @@ class HomeFragment : Fragment() {
             adapter = foundCitiesAdapter
             layoutManager = LinearLayoutManager(this@HomeFragment.requireContext())
         }
+        val today = Calendar.getInstance().time
+        val todayStr = DateFormat.getDateInstance(DateFormat.MEDIUM).format(today)
+        binding.tvTitle.text = "Hello. Today is $todayStr"
     }
 
     private fun setupListeners() {
